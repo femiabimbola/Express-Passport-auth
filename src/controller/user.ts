9921f653ae93e49da98ca2e0f0e7e46342b565db
femi abimbola <femiabimbola6@gmail.com>
2024-11-03 12:42:57 -0800

    seed a user in the database|

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,14 +1,35 @@
 import { Request, Response, NextFunction, Router } from "express";
 import { validationResult, matchedData } from "express-validator";
+import { createUserModel} from "../model/userModel";
 
 export const createUser = async (req: Request, res: Response) => {
-  const result = validationResult(req);
-  if (!result.isEmpty()) {
-    return res.status(400).send({ error: result.array().map((err) => err) });
+  // const result = validationResult(req);
+  // if (!result.isEmpty()) {
+  //   return res.status(400).send({ error: result.array().map((err) => err) });
+  // }
+  // const data = matchedData(req);
+  const {
+    firstName, lastName, email, password, phone, address,
+  } = req.body;
+  const newUser = {
+    firstName,lastName,email,
+    password,
+    phone,
+    address,
+    status: 'unverified',
+    isAdmin: false,
   }
-  const data = matchedData(req);
-  return res.status(201).send({ msg: "You have successfully log in" });
+  console.log(newUser)
+  try {
+    const userObject = await createUserModel(newUser)
+    return res.status(201).send({ msg: "You have successfully log in" });
+  } catch (error) {
+    return res.status(201).send({ msg: "Could not create user" });
+  }
+ 
 };
+
+
 
 export const signIn = async (req: Request, res: Response) => {
   return res.status(201).send({ msg: "You have successfully log in" });
---
+++ src/model/userModel.ts
@@ -0,0 +1,34 @@
+import { client } from "../db";
+
+export const createUserModel = (params: any) =>{
+  try {
+    const {firstName, lastName, email, password, phone, address, status, isAdmin,} = params;
+    const newUser = [
+      firstName,
+      lastName,
+      email,
+      password,
+      phone,
+      address,
+      status,
+      isAdmin,
+    ];
+    const queryText = 'INSERT INTO users (firstname, lastname, email, password, phone, address, status, isAdmin) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *';
+    client.connect()
+    .then(() => {
+      console.log("connecred to postgres database")
+      client.query(queryText, newUser,(err, result) => 
+        {
+        if (err) {
+          console.error("Error inserting data", err);
+        } else {
+          console.log("User created successfully");
+        }
+  
+        client.end();
+      })
+    })
+  } catch (error) {
+    console.log(`error occured creating user ${error}`)
+  }
+}
