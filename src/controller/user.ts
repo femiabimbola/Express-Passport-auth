575d4d1942c14469f81f7e6069bf852b037bc6dc
Femi Abimbola <femiabimbola6@gmail.com>
2024-11-05 16:50:12 +0100

    working on getting a single user

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction, Router } from "express";
 import { validationResult, matchedData } from "express-validator";
-import { createUserModel, getAllUserModel } from "../model/userModel";
+import { createUserModel, getAllUserModel, getAUserModel } from "../model/userModel";
 
 export const createUser = async (req: Request, res: Response) => {
   const result = validationResult(req);
@@ -8,7 +8,6 @@ export const createUser = async (req: Request, res: Response) => {
     return res.status(400).send({ error: result.array().map((err) => err) });
   }
   const data = matchedData(req);
-  console.log(data);
   const { firstName, lastName, email, password, phone, address } = req.body;
   const newUser = {
     firstName,
@@ -22,7 +21,7 @@ export const createUser = async (req: Request, res: Response) => {
   };
   try {
     const userObject = await createUserModel(newUser);
-    return res.status(201).send({ msg: "You have successfully log in" });
+    return res.status(201).send({ msg: "You have successfully created", data: userObject });
   } catch (error) {
     return res.status(201).send({ msg: "Could not create user" });
   }
@@ -35,7 +34,17 @@ export const signIn = async (req: Request, res: Response) => {
 export const getAllUser = async (req: Request, res: Response) => {
   try {
     const userObject = await getAllUserModel();
-    return res.status(200).send({ data: userObject, message: "You have all your useer" });
+    return res.status(200).send({ message: "You have all your useer", data: userObject });
+  } catch (error) {
+    return res.status(200).send({ message: "Could not get the user" });
+  }
+};
+
+export const getAUser = async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id, 10);
+  try {
+    const userObject = await getAUserModel(id);
+    return res.status(200).send({ message: "You have a your useer", data: userObject });
   } catch (error) {
     return res.status(200).send({ message: "Could not get the user" });
   }
