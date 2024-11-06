91e8ea056bcdaaecac8183b3a3206e86edb05594
Femi Abimbola <femiabimbola6@gmail.com>
2024-11-06 17:11:01 +0100

    loan model

--- src/model/loanModel.ts
+++ src/model/loanModel.ts
@@ -26,11 +26,9 @@ export const createloanModel = (params: any) => {
 
 export const getAllloanModel = async () => {
   try {
-    await client.connect().then(() => console.log("connected to postgres database"));
-
-    const result = await client.query("SELECT * FROM users ORDER BY id ASC");
-
-    // client.end().then(() => console.log("Connection to PostgreSQL closed"));
+    await pool.connect().then(() => console.log("database connected"));
+    const result = await pool.query("SELECT * FROM users ORDER BY id ASC");
+    pool.end();
     return result.rows;
   } catch (error) {
     console.log(`error occured getting all user ${error}`);
