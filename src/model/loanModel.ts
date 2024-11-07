5c54eb2e7893a4a7dbf53432f91d9bf101d6511f
femi abimbola <femiabimbola6@gmail.com>
2024-11-07 11:38:44 -0800

    refactoring

--- src/model/loanModel.ts
+++ src/model/loanModel.ts
@@ -1,24 +1,14 @@
-import { client } from "../db";
+
 import { pool } from "../db";
 
-export const createloanModel = (params: any) => {
+export const createloanModel = async (params: any) => {
   try {
     const { firstName, lastName, email, password, phone, address, status, isAdmin } = params;
     const newUser = [firstName, lastName, email, password, phone, address, status, isAdmin];
     const queryText =
       "INSERT INTO loans (firstname, lastname, email, password, phone, address, status, isAdmin) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *";
-    client.connect().then(() => {
-      console.log("connected to postgres database");
-      client.query(queryText, newUser, (err, result) => {
-        if (err) {
-          console.error("Error inserting data", err);
-        } else {
-          console.log("User created successfully");
-        }
-
-        client.end();
-      });
-    });
+      const result = await pool.query(queryText, newUser);
+      return result.rows;
   } catch (error) {
     console.log(`error occured creating user ${error}`);
   }
@@ -26,15 +16,9 @@ export const createloanModel = (params: any) => {
 
 export const getAllloanModel = async () => {
   try {
-    // await pool.connect().then(() => console.log("database connected"));
     const result = await pool.query("SELECT * FROM users ORDER BY id ASC");
-    // pool.end();
     return result.rows;
   } catch (error) {
     console.log(`error occured getting all user ${error}`);
-  } finally {
-    client.end().then(() => {
-      console.log("Connection to PostgreSQL closed");
-    });
-  }
+  } 
 };
--- src/model/userModel.ts
+++ src/model/userModel.ts
@@ -1,4 +1,4 @@
-import { client } from "../db";
+
 import { pool } from "../db";
 
 export const createUserModel = async (params: any) => {
@@ -7,32 +7,20 @@ export const createUserModel = async (params: any) => {
   const queryText =
     "INSERT INTO users (firstname, lastname, email, password, phone, address, status, isAdmin) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *";
   try {
-    // await client.connect().then(() => console.log("connected to postgres database"));
     const result = await pool.query(queryText, newUser);
-    // client.end().then(() => console.log("Connection to PostgreSQL closed"));
     return result.rows;
   } catch (error) {
     console.log(`error occured creating user ${error}`);
-  } finally {
-    client.end().then(() => {
-      console.log("Connection to PostgreSQL closed");
-    });
-  }
+  } 
 };
 
 export const getAllUserModel = async () => {
   try {
     const result = await pool.query("SELECT * FROM users ORDER BY id ASC");
-
-    // client.end().then(() => console.log("Connection to PostgreSQL closed"));
     return result.rows;
   } catch (error) {
     console.log(`error occured getting all user ${error}`);
-  } finally {
-    client.end().then(() => {
-      console.log("Connection to PostgreSQL closed");
-    });
-  }
+  } 
 };
 
 export const getAUserModel = async (id: number) => {
@@ -46,26 +34,3 @@ export const getAUserModel = async (id: number) => {
     console.log(`error occured getting a user ${error}`);
   }
 };
-
-// export const createUserModel = (params: any) => {
-//   try {
-//     const { firstName, lastName, email, password, phone, address, status, isAdmin } = params;
-//     const newUser = [firstName, lastName, email, password, phone, address, status, isAdmin];
-//     const queryText =
-//       "INSERT INTO users (firstname, lastname, email, password, phone, address, status, isAdmin) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *";
-//     client.connect().then(() => {
-//       console.log("connected to postgres database");
-//       client.query(queryText, newUser, (err, result) => {
-//         if (err) {
-//           console.error("Error inserting data", err);
-//         } else {
-//           console.log("User created successfully");
-//         }
-
-//         client.end();
-//       });
-//     });
-//   } catch (error) {
-//     console.log(`error occured creating user ${error}`);
-//   }
-// };
