3f7f919b2f62830b3997144deea9be881a49d4ee
femi abimbola <femiabimbola6@gmail.com>
2024-11-21 17:19:54 -0800

    fixing the passport login

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,11 +3,13 @@ import { checkSchema } from "express-validator";
 import { validationResult, matchedData } from "express-validator";
 import { createUser, signIn, getAllUser } from "../controller/user";
 import { createUserValidationSchema } from "../validation/user";
+import passport from "passport";
+import "../utils/passportStrategy/localStrategy"
 
 const router = Router();
 
 router.post("/api/auth/register", checkSchema(createUserValidationSchema), createUser);
-router.get("/api/auth/login",signIn)
+router.post("/api/auth/login", passport.authenticate("local"),signIn)
 router.get("/api/auth/users", getAllUser);
 
 export default router;
--- src/utils/passportStrategy/localStrategy.ts
+++ src/utils/passportStrategy/localStrategy.ts
@@ -1,14 +1,15 @@
 import passport from 'passport'
 import { Strategy } from 'passport-local'
+import bcrypt from "bcryptjs"
 import { findAUserByEmail, findAUserByID } from '../../model/userModel';
 
-// The serialize function takes the user object and stores it in the session.it get called when user sign in
+// The serialize function takes the user object and stores it in the session.it get called during user sign in
 passport.serializeUser((user:any, done) => {
   console.log(`inside serialize user`);
   done(null, user.id)
 })
 
-// The deserialize function take the id from session, and find who the user is.it get called when user sign in and call other routes
+// The deserialize function take the id from session, and find who the user is.it get called afteruser sign in and call other routes
 passport.deserializeUser(async (id: string, done) => {
   
   console.log(`inside deserialize user ${id}`);
@@ -25,8 +26,9 @@ export default passport.use(
   new Strategy({usernameField:"email"}, async (username, password, done) =>{
     try{
       const findUser = await findAUserByEmail(username);
-      if(!findUser) throw new Error("user not found") // Error is move to the catch block
-      if(findUser.password !== password) throw new Error("Invalid Password")
+      if(!findUser) throw new Error("user not found") 
+      const passwordsMatch = await bcrypt.compare(password, findUser.password); 
+      if(!passwordsMatch) throw new Error("Invalid Password")
       // The done has 3 arg! Check the docs
       done(null, findUser)
     }catch(error){
